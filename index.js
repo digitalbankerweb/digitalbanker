ea2031eb4552843fe4e826f35d73fc6e32fb8647
digitalbankerweb <digitalbankerweb@gmail.com>
2024-08-25 12:07:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+
+/*
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/digitalbankerweb/digitalbanker.git
+git push -u origin main
+
+
+*/
